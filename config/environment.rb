2fdd2c1264504bf327e6eb02b1964c4306d28271
Bois <tkaboris@gmail.com>
2015-11-30 09:06:23 -0800

    mm

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,8 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :user_name      => ENV['tkaboris'],
-  :password       => ENV['platonovna1949'],
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
   :domain => 'heroku.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
